68691b2dcbbdfd686f964d919b886cb27da2fe74
allisa liberto <allisa84@yahoo.com>
2018-06-11 17:59:55 -0500

    fixed functionality

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,32 +2,101 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AnimalCard from "./components/AnimalCard";
+import Wrapper from "./components/Wrapper";
+import animal from "./animals.json";
+import Nav from "./components/Navbar";
+import Title from "./components/Title";
+import Column from "./column.js";
+
+function shuffleAnimals(array) {
+  for (let i = array.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
 
 class App extends Component {
 
     state = {
-      animals
+      animal,
+      currentScore: 0,
+      topScore: 0,
+      rightWrong: "",
+      clicked: [],
     };
 
-  removeAnimal = id => {
-    const animals = this.state.animals.filter(animal => animal.id !== id);
-    this.setState({ animals });
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id)});
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+      const newScore = this.state.currentScore + 1;
+      this.setState({
+        currentScore: newScore,
+        rightWrong: ""
+       });
+       if (newScore >= this.state.topScore) {
+         this.setState({ topScore: newScore });
+       } else if (newScore === 12) {
+         this.setState({ rightWrong: "You Won!" });
+       }
+       this.handleShuffle();
+    };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Reset Game",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledAnimals = shuffleAnimals(animal);
+    this.setState({ animals: shuffledAnimals });
   };
 
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the clicky game!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      
+    <Wrapper>
+      <Nav
+      title="Animal Clicky Game"
+      score={this.state.currentScore}
+      topScore={this.state.topScore}
+      rightWrong={this.state.rightWrong}
+      />
+    
+    <Title>
+      Try to click on each animal only once, click twice and you will reset the game.
+    </Title>
+
+      {this.state.animal.map(animal => (
+        <Column size="md-3 sm-6">
+        <AnimalCard
+        id={animal.id}
+        key={animal.id}
+        name={animal.name}
+        image={animal.image}
+        handleClick={this.handleClick}
+        handleIncrement={this.handleIncrement}
+        handleReset={this.handleReset}
+        handleShuffle={this.handleShuffle}
+        />
+      </Column>
+      )
+    )}
+    </Wrapper>
+    )};
   }
-}
 
 export default App;
--- clicky-game/src/components/AnimalCard/AnimalCard.js
+++ clicky-game/src/components/AnimalCard/AnimalCard.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./AnimalCard.css";
 
 const AnimalCard = props => (
-    <div className="card">
+    <div className="card" value={props.id} onClick={() => props.handleClick(props.id)}>
         <div className="img-container">
-            <img onClick={() => props.shuffleAnimals(props.id)} alt={props.name} src={props.image} />
+            <img alt={props.name} src={props.image} />
         </div>
         <div className="content">
             <strong>Name: </strong> {props.name}
         </div>
     </div>
 )
+
+export default AnimalCard;
